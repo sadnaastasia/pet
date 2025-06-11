a4ca79882cb347a4f20a8b428d625b96565c4999
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2025-06-11 09:45:10 +0300

    adaptive article

--- src/components /MainArticle.tsx
+++ src/components /MainArticle.tsx
@@ -2,7 +2,7 @@ function MainArticle() {
   return (
     <div className="mainArticle">
       <h1>
-        Theory and Practice of Cognitive-Behavioural Therapy: a clinical course
+        Theory and Practice of Cognitive-Behavioral Therapy: a clinical course
         with pre-accreditation status
       </h1>
       <div className="mainArticle_buttons">
