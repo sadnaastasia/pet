0bc1ae937fe2094e728b0d3eb74ddad4ed138de6
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2025-06-13 09:21:42 +0300

    card with video

--- src/components /AboutMainCourse.tsx
+++ src/components /AboutMainCourse.tsx
@@ -2,7 +2,16 @@ function AboutMainCourse() {
   return (
     <div className="aboutMainCourse_container">
       <div className="aboutMainCourse-banner">
-        <div className="aboutMainCourse-banner_item1"></div>
+        <div className="aboutMainCourse-banner_item1">
+          <div className="aboutMainCourse-banner_item1_background">
+            <div className="aboutMainCourse-banner_item1_videoContainer">
+              <video autoPlay muted loop>
+                <source src="../../public/video.mp4" type="video/mp4" />
+              </video>
+              <button className="aboutMainCourse_button">Try for free</button>
+            </div>
+          </div>
+        </div>
         <div className="aboutMainCourse-banner_item2"></div>
         <div className="aboutMainCourse-banner_item3"></div>
         <div className="aboutMainCourse-banner_item4"></div>
