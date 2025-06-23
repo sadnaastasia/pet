f84349cfeacf0bf44240d30846edc0fc2fb50cd9
Anastasiya Staratsitarava <starotitorova.anastasia@mail.ru>
2025-06-23 13:09:49 +0300

    correct styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ const Wrapper = ({ children }: { children: React.ReactElement }) => {
   const location = useLocation();
 
   useLayoutEffect(() => {
-    // Scroll to the top of the page when the route changes
     window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
   }, [location.pathname]);
 
